01333125804acfbb64b4aef3229f17bda8130f2b
MeteoricDragon <MattRohde@gmail.com>
2021-10-05 13:36:19 -0400

    Establish LocalDB connection & connectionstring, migrations, context, repository

---
+++ Migrations/20210923190242_memberTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LogicalEngineAngular.Migrations
+{
+    public partial class memberTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "members",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartCode = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    UnitsOwned = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_members", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "members");
+        }
+    }
+}
---
+++ Models/CarPartUnitContainerMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogicalEngineAngular.Models
+{
+    public class CarPartUnitContainerMember
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string PartCode { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string UnitsOwned { get; set; }
+    }
+}
